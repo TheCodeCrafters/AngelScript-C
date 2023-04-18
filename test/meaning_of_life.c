bd0c17a1a824e0b38cad8c9094ca35a56521f916
ENDERZOMBI102 <enderzombi102.end@gmail.com>
2023-04-18 14:28:57 +0200

    setup testing with ctest
    ~ Firmament

--- test/meaning_of_life.c
+++ test/meaning_of_life.c
@@ -9,6 +9,7 @@
 
 void messageCallback( const asSMessageInfo* msg, void* obj ) {
 	printf( "Received message from VM: `%s`\n", msg->message );
+	(void) obj;
 }
 
 void print( const int input ) {
@@ -37,13 +38,18 @@ int main() {
 	asIScriptFunction* func = asModule_GetFunctionByName( asEngine_GetModule( engine, "app", asGM_ONLY_IF_EXISTS ), "main" );
 	TRY(asContext_Prepare( ctx, func ));
 	r = asContext_Execute( ctx );
+
+	int code = -1;
 	if ( r == asEXECUTION_FINISHED ) {
-		printf( "return value: %lu", asContext_GetReturnDWord( ctx ) );
+		code = asContext_GetReturnWord( ctx );
+		printf( "return value: %d", code );
 	}
-	TRY(asContext_Release( ctx ));
 
 	// shutdown
+	TRY(asContext_Release( ctx ));
 	TRY(asEngine_ShutdownAndRelease( engine ));
+
+	return code;
 }
 
 
